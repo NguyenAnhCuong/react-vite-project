8c832cd64247f34a833a3c55b4978476428f69f8
cuong <cuong1606x@gmail.com>
2025-05-17 20:22:45 +0700

    fix language

--- src/components/utils/i18n.jsx
+++ src/components/utils/i18n.jsx
@@ -5,18 +5,17 @@ import Backend from "i18next-http-backend";
 
 i18n
   .use(Backend)
-  // detect user language
-  // learn more: https://github.com/i18next/i18next-browser-languageDetector
   .use(LanguageDetector)
-  // pass the i18n instance to react-i18next.
   .use(initReactI18next)
-  // init i18next
-  // for all options read: https://www.i18next.com/overview/configuration-options
   .init({
     debug: false,
     fallbackLng: "en",
+    backend: {
+      // Lưu ý dùng import.meta.env.BASE_URL nếu đang dùng Vite
+      loadPath: `${import.meta.env.BASE_URL}locales/{{lng}}/{{ns}}.json`,
+    },
     interpolation: {
-      escapeValue: false, // not needed for react as it escapes by default
+      escapeValue: false,
     },
   });
 
