b64824992b34c8209cba46433b629dec0d583898
cuong <cuong1606x@gmail.com>
2025-05-17 20:15:54 +0700

    fix 5

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,14 @@
 import ReactDOM from "react-dom/client";
 import "swiper/css";
-import 'swiper/css/navigation';
-import 'swiper/css/pagination';
+import "swiper/css/navigation";
+import "swiper/css/pagination";
 import "./index.css";
 import "nprogress/nprogress.css";
 import "react-perfect-scrollbar/dist/css/styles.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "react-18-image-lightbox/style.css";
 import "react-image-gallery/styles/css/image-gallery.css";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import Layout from "./Layout.jsx";
 import { Provider } from "react-redux";
 import { store, persistor } from "./redux/store.jsx";
@@ -19,9 +19,9 @@ ReactDOM.createRoot(document.getElementById("root")).render(
   // <React.StrictMode>
   <Provider store={store}>
     <PersistGate loading={null} persistor={persistor}>
-      <BrowserRouter>
+      <HashRouter>
         <Layout />
-      </BrowserRouter>
+      </HashRouter>
     </PersistGate>
   </Provider>
 
